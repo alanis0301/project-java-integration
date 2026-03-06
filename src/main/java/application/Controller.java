737b715a0707b5044a18a6c2e8212d8c4e0a61a9
alanis <nogueiraalanis6@gmail.com>
2026-03-06 10:12:01 -0300

    feat: projeto 03

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -7,90 +7,70 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.TitledPane;
 
+import models.Line;
+import models.Category;
+import models.Model;
+import controllers.BackEndController;
+
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
-    private ComboBox<String> comboLinhas;
+    private ComboBox<Line> comboLines;
 
     @FXML
-    private TreeView<String> treeModelos;
+    private TreeView<String> treeModels;
 
     @FXML
-    private TitledPane paneModelos;
+    private TitledPane paneModels;
+
+    private BackEndController backEndController;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        backEndController = new BackEndController();
 
-        paneModelos.setDisable(true);
-
-        comboLinhas.getItems().addAll("Cronos", "Ares");
-
-        comboLinhas.setOnAction(event -> {
-            String linha = comboLinhas.getValue();
-
-            if (linha != null) {
-                paneModelos.setDisable(false);
-                carregarTree(linha);
-            }
-        });
+        configureInitialState();
+        configureCombo();
     }
 
-    private void carregarTree(String linha) {
+    private void configureInitialState() {
+        paneModels.setDisable(true);
+    }
 
+    private void configureCombo() {
+        List<Line> lines = backEndController.getLines();
+
+        comboLines.getItems().addAll(lines);
+    }
+
+    @FXML
+    private void onLineSelect() {
+        Line line = comboLines.getValue();
+
+        if (line != null) {
+            paneModels.setDisable(false);
+            loadTree(line);
+        }
+    }
+
+    private void loadTree(Line line) {
         TreeItem<String> root = new TreeItem<>("Categorias");
         root.setExpanded(true);
 
-        if (linha.equals("Cronos")) {
+        for (Category category : line.getCategories()) {
+            TreeItem<String> categoryItem =
+                    new TreeItem<>(category.getName());
 
-            TreeItem<String> cronosOld = new TreeItem<>("Cronos Old");
-            cronosOld.getChildren().addAll(
-                    new TreeItem<>("Cronos6001-A"),
-                    new TreeItem<>("Cronos 6003"),
-                    new TreeItem<>("Cronos 7023")
-            );
-
-            TreeItem<String> cronosL = new TreeItem<>("Cronos L");
-            cronosL .getChildren().addAll(
-                    new TreeItem<>("Cronos 6021L"),
-                    new TreeItem<>("Cronos 7023L")
-            );
-
-            TreeItem<String> cronosNG = new TreeItem<>("Cronos-NG");
-            cronosNG.getChildren().addAll(
-                    new TreeItem<>("Cronos 6001-NG"),
-                    new TreeItem<>("Cronos 6003-NG"),
-                    new TreeItem<>("Cronos 6021-NG"),
-                    new TreeItem<>("Cronos 6031-NG"),
-                    new TreeItem<>("Cronos 7021-NG"),
-                    new TreeItem<>("Cronos 7023-NG")
-            );
-
-            root.getChildren().addAll(cronosOld, cronosL, cronosNG);
+            for (Model model : category.getModels()) {
+                categoryItem.getChildren()
+                        .add(new TreeItem<>(model.getName()));
+            }
+            root.getChildren().add(categoryItem);
         }
-
-
-        if (linha.equals("Ares")) {
-
-            TreeItem<String> aresTB = new TreeItem<>("Ares TB");
-            aresTB.getChildren().addAll(
-                    new TreeItem<>("ARES 7021"),
-                    new TreeItem<>("ARES 7031"),
-                    new TreeItem<>("ARES 7023")
-            );
-
-            TreeItem<String> aresTHS = new TreeItem<>("Ares THS");
-            aresTHS.getChildren().addAll(
-                    new TreeItem<>("ARES 8023 15"),
-                    new TreeItem<>("ARES 8023 200"),
-                    new TreeItem<>("ARES 8023 2,5")
-            );
-
-            root.getChildren().addAll(aresTB, aresTHS);
-        }
-
-        treeModelos.setRoot(root);
+        treeModels.setRoot(root);
     }
 }
