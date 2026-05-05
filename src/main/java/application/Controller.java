e666a8b49e6eda6523791eb24a41bf8d01aafc43
alanis <nogueiraalanis6@gmail.com>
2026-05-05 10:19:49 -0300

    Projeto 4 - API

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,5 +1,6 @@
 package application;
 
+import controllers.LineRepository;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -16,6 +17,7 @@ import services.ApiLineService;
 import java.util.List;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,16 +13,19 @@ public class Main extends Application {
 
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("/Main.fxml")
-        );
+        ); 
 
         Scene scene = new Scene(loader.load());
 
         stage.setScene(scene);
         stage.setTitle("Projeto de Integração");
+        stage.getIcons().add(
+                new Image(getClass().getResourceAsStream("/assets/project-java-integration.png"))
+        );
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
